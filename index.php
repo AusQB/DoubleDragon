ac03870cbac3cd83175ab558d7dd2dff2902d0a4
Robert Gillman <rgillman88@gmail.com>
2016-01-28 19:05:27 +0800

    Added meta tags

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ $nouns = file('nouns.txt');
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Team Double Dragon Name Generator</title>
 
 <style type="text/css">
 
